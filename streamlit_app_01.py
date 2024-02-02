b45079f0ec074887867ecfb4c20f859de0d77d86
NadavAharoni <65665296+NadavAharoni@users.noreply.github.com>
2024-02-02 08:44:45 +0200

    Added streamlit_app_01.py
    Application with login form.
    Authentication and sign up with Supabase

---
+++ streamlit_app_01.py
@@ -0,0 +1,115 @@
+import os
+import streamlit as st
+import supabase
+from supabase import create_client, Client
+
+def logout_form(supabase_session):
+    with st.form('log out'):
+        st.write(F"{supabase_session.user.email}")
+        submit = st.form_submit_button('log out')
+
+    if submit:
+        st.session_state.supabase_client.auth.sign_out()
+        st.rerun()
+
+def signup_form():
+    st.title("sign up")
+    with st.form('sign up'):
+        signup_username = st.text_input(label='username', key='signup_username') # label_visibility="hidden"
+        signup_password = st.text_input(label='password', key='signup_password', type='password') # label_visibility="hidden"
+        submit = st.form_submit_button('sign up')
+
+    if submit:
+        print('sign up submitted')
+        st.write(F'username={signup_username}')
+        credentials = {}
+        credentials['email'] = signup_username
+        credentials['password'] = signup_password
+        st.write(F'credentials={credentials}')
+        res = st.session_state.supabase_client.auth.sign_up(credentials)
+        st.write(F"{res}")
+        # st.rerun() ?
+
+def login_form():
+    st.title("log in")
+    with st.form('log in'):
+        c1, c2  = st.columns([1, 1])
+        username =c1.text_input('username', key='username') # label_visibility="hidden"
+        password = c1.text_input('password', key='password', type='password') # label_visibility="hidden"
+        submit = st.form_submit_button('log in')
+
+    if submit:
+        st.write(F'username={username}')
+        credentials = {}
+        credentials['email'] = username
+        credentials['password'] = password
+        user = st.session_state.supabase_client.auth.sign_in_with_password(credentials)
+        st.rerun()
+
+def button_clicked():
+    print('button clicked')
+    if st.session_state.form_to_show == 'login':
+        print('login -> signup')
+        st.session_state.form_to_show = 'signup'
+    else:
+        print('signup -> login')
+        st.session_state.form_to_show ='login'
+
+def show_data():
+    rows = st.session_state.supabase_client.table("Students").select("*").execute()
+    st.dataframe(rows.data)
+
+    # for row in rows.data:
+    #     st.write(f"{row['first_name']} {row['last_name']}")
+
+
+    
+
+def main():
+    st.set_page_config(layout="wide")
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase_client: Client = create_client(url, key)
+    st.session_state.supabase_client = supabase_client
+    st.session_state.user = supabase_client.auth.get_user()
+
+    # st.session_state.form_to_show = "login"
+    if "form_to_show" not in st.session_state:
+        st.session_state.form_to_show = 'signup' 
+
+    with st.sidebar:
+        st.title("Welcome!")
+        if st.session_state.supabase_client:
+            # supabase_session = st.session_state.supabase_client.auth.get_user()
+            supabase_session = st.session_state.supabase_client.auth.get_session()
+            if supabase_session:
+                logout_form(supabase_session)
+            else:
+                st.write("log in or sign up to start")
+
+                if st.session_state.form_to_show == 'signup':
+                    button_label = 'log in'
+                else:
+                    button_label = 'sign up'
+
+                st.button(button_label, on_click=button_clicked)
+
+        # st.markdown(F"###### form to show={st.session_state.form_to_show}")
+
+    # main pane
+    if st.session_state.supabase_client:
+        supabase_session = st.session_state.supabase_client.auth.get_session()
+        if supabase_session:
+            show_data()
+        else:
+            if st.session_state.form_to_show == 'signup':
+                signup_form()
+            else:
+                login_form()
+
+    # st.write(F"{supabase_client.rest_url}")
+
+
+if __name__ == "__main__":
+    main()
